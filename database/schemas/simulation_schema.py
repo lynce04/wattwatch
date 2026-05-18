c8036fae1e14dc4a2c4999de3160457eb1a65506
Lynce Talla <lyncejorelt@gmail.com>
2026-05-18 13:51:03 +0200

    added Schema for simulations

---
+++ database/schemas/simulation_schema.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class SimulationBase(BaseModel):
+    name: str
+    description: str
+    scenario:str
+
+class SimulationResponse(SimulationBase):
+    id:int
+    created_at:datetime
+
+    class Config:
+        from_attributes = True
