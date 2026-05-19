2cce79faa93a9f05696d58c97c348b6d6f9705b6
Lynce Talla <lyncejorelt@gmail.com>
2026-05-20 00:23:30 +0200

    edited the service to include pagination and date filter

--- app/api/smard.py
+++ app/api/smard.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
-from fastapi import Depends
+from fastapi import Depends ,HTTPException ,APIRouter
+from sqlalchemy.orm import Session
 from database.session import get_db
-from app.services.smard_data_service import import_smard_data
+from app.services.smard_data_service import import_smard_data , get_all_smard_data,get_smard_by_id
 
 router = APIRouter(prefix="/api/smard" , tags=["SMARD"])
 
@@ -9,3 +9,12 @@ router = APIRouter(prefix="/api/smard" , tags=["SMARD"])
 async def import_data(filter_id: int, region: str,db= Depends(get_db)):
     result= await import_smard_data(filter_id,region,db)
     return  {"inserted_rows": result}
+@router.get("/")
+async  def get_all_smards(db = Depends(get_db),skip: int = 0,limit: int = 10 ,from_date:int =None,to_date:int =None):
+     return get_all_smard_data(db, skip, limit, from_date,to_date)
+
+@router.get("/{id}")
+async def get_data_by_id(id: int, db= Depends(get_db)):
+    result = get_smard_by_id(id,db)
+    if result is None: raise HTTPException(status_code=404, detail="Smard data not found")
+    return result
--- app/services/smard_data_service.py
+++ app/services/smard_data_service.py
@@ -17,3 +17,17 @@ async def import_smard_data(filter_id,region,db):
     db.commit()
 
     return count
+
+def get_all_smard_data(db,skip:int=0,limit:int=10,from_date:int=None,to_date:int=None):
+    query=db.query(SmardData)
+
+    if from_date:
+        query = query.filter(SmardData.timestamp >= from_date)
+    if to_date:
+        query= query.filter(SmardData.timestamp <= to_date)
+
+    query = query.offset(skip).limit(limit).all()
+    return query
+
+def get_smard_by_id(id,db):
+    return db.query(SmardData).filter(SmardData.id == id).first()
