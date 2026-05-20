72e73562c7bc435a8fa63d31a42f2df529cbc8be
Lynce Talla <lyncejorelt@gmail.com>
2026-05-20 13:49:45 +0200

    edited the time representation of the simulationresponse

--- database/schemas/simulation_schema.py
+++ database/schemas/simulation_schema.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel,field_serializer
+
+
 
 class SimulationBase(BaseModel):
     name: str
@@ -10,8 +12,12 @@ class SimulationBase(BaseModel):
     scenario:str
 
 class SimulationResponse(SimulationBase):
-    id:int
-    created_at:datetime
+    id: int
+    created_at: datetime
+
+    @field_serializer('created_at')
+    def format_created_at(self, value: datetime) -> str:
+        return value.strftime("%Y-%m-%d %H:%M:%S UTC")
 
     class Config:
         from_attributes = True
